f0a983912c87fe179a812811eb4271e108245daa
Niidaddey2025 <sdardey1221@gmail.com>
2025-06-25 13:06:26 +0000

    netlify router fix

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -1,4 +1,5 @@
 // const BASE_URL = 'https://data-package-backend.onrender.com';
-const BASE_URL = 'http://localhost:5000';
+// const BASE_URL = 'http://localhost:5000';
 
+const BASE_URL = 'https://kelishubbackend-production.up.railway.app';
 export default BASE_URL;
