5112b3de17ac57fcc34d66ffeaf2d6bcc5bb4abc
Niidaddey2025 <sdardey1221@gmail.com>
2025-06-26 15:08:58 +0000

    using railway for hackend hosting

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -1,7 +1,6 @@
 // const BASE_URL = 'https://data-package-backend.onrender.com';
 // const BASE_URL = 'http://localhost:5000'; -- Use this for local development
-// const BASE_URL = 'https://kelishubbackend-production.up.railway.app';
 
-const BASE_URL = 'https://kelisbackend.vercel.app';
+const BASE_URL = 'https://datapackage-production.up.railway.app';
 
 export default BASE_URL;
