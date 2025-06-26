5f0fb94748b99b8550e4d1fb880ac898880d564c
Niidaddey2025 <sdardey1221@gmail.com>
2025-06-26 14:22:30 +0000

    finished

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -1,5 +1,7 @@
 // const BASE_URL = 'https://data-package-backend.onrender.com';
-// const BASE_URL = 'http://localhost:5000';
+// const BASE_URL = 'http://localhost:5000'; -- Use this for local development
+// const BASE_URL = 'https://kelishubbackend-production.up.railway.app';
 
-const BASE_URL = 'https://kelishubbackend-production.up.railway.app';
+const BASE_URL = 'https://kelisbackend.vercel.app';
+
 export default BASE_URL;
