1484c5817b16cad1ca66918af1f03b72cef2b66c
Niidaddey2025 <sdardey1221@gmail.com>
2025-06-30 16:18:26 +0000

    fixed announcement

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -1,6 +1,5 @@
-// const BASE_URL = 'https://data-package-backend.onrender.com';
-const BASE_URL = 'http://localhost:5000';
+//const BASE_URL = 'http://localhost:5000';
 
-// const BASE_URL = 'https://datapackage-production.up.railway.app';
+const BASE_URL = 'https://datapackage-production.up.railway.app';
 
 export default BASE_URL;
